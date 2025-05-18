f45a08df7958a33dcec9211762e5c6d7eb948400
picspiii1234 <ibrihim539@gmail.com>
2025-05-18 14:44:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+// for(let i = 1; i < 101; i++)
+
+//     console.log(i);
+    
+
+
+// let i = 0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+    
+// }
+
+// let mevalar = ['olma' , 'gilos' , 'anor' , 'shoftoli'];
+
+
+// for(let i = 0; i < mevalar.length; i++ )
+
+//     console.log(i);
+    
+
+// let sonlar = [13,56,89,16,56,87,67,97,34,56,76,56,];
+
+// for (let i = 2; i < sonlar.length; i++) {
+//     if(sonlar [i]  ===  2){
+//         console.log(sonlar[i]);
+        
+//     }
+    
+// }
+
+
+
+let son = [2,7,90,14,46,31,43,24,78,99,76,43,23,];
+
+console.log(son);
+
+
+let sonlar = [2,7,90,14,46,31,43,24,78,99,76,43,23,];
+
+
+
+
+for (let i = 0; i < sonlar.length; i++) {
+   if(sonlar [i] % 2 == 0){
+
+
+   
+     
+  console.log(sonlar[i]);
+   }
+    
+
+}
+  
